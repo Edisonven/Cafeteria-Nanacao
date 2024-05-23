dcaf19fe820a85c9f41dc4c7d37c8a004506b0b5
Edison <aalevenegass@gmail.com>
2024-05-23 16:41:35 -0400

    Creando test para ruta get de cafes

--- Backend/tests/server.spec.js
+++ Backend/tests/server.spec.js
@@ -2,5 +2,11 @@ const request = require("supertest");
 const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
-
+  it("testeando ruta /cafes con statusCode 200", async () => {
+    const { statusCode, body: cafes } = await request(server)
+      .get("/cafes")
+      .send();
+    expect(statusCode).toBe(200);
+    expect(cafes).toBeInstanceOf(Array);
+  });
 });
